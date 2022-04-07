f52e17e7390ed76d23cb4dd3cb0f755d55f354e6
Sagar Adhikari <sagradhkr48@gmail.com>
2022-04-07 21:30:35 +0545

    financial -cloud module

---
+++ src/app/financial-cloud/financial-cloud.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-financial-cloud',
+  templateUrl: './financial-cloud.component.html',
+  styleUrls: ['./financial-cloud.component.scss'],
+})
+export class FinancialCloudComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/financial-cloud/financial-cloud.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FinancialCloudRoutingModule } from './financial-cloud-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FinancialCloudRoutingModule
+  ]
+})
+export class FinancialCloudModule { }
