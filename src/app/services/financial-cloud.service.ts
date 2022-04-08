24ce509d8c45ebb3aefc814d10fd29b42d40a1f8
Sagar Adhikari <sagradhkr48@gmail.com>
2022-04-08 12:26:54 +0545

    minor

--- src/app/services/financial-cloud.service.ts
+++ src/app/services/financial-cloud.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IPokeApi } from '../utils/pokeApi.interfaces';
+
 import { GlobalService } from './global.service';
 import { RestApiService } from './rest-api.service';
 
