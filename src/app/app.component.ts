29ca67b3d123c8e732cb6a1f20a2301795b48e64
Sagar Adhikari <sagradhkr48@gmail.com>
2022-04-08 12:50:09 +0545

    financial cloud package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { GlobalService } from './services/global.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'financialCloud';
+  isOpenSideNavBar = true;
+  loading = false;
+  pageTitle = 'Initial Title';
+  allowFooter = true;
+  smallScreen = false;
+
+  constructor(private globalService: GlobalService) {
+    this.globalService.pageTitle$.subscribe((x) => {
+      console.log('layout', x);
+      this.pageTitle = x.pageTitle;
+      this.allowFooter = x.allowFooter;
+    });
+
+    this.globalService.loading$.subscribe((x) => {
+      this.loading = x;
+    });
+  }
+
+  onNavbarClicked() {
+    this.isOpenSideNavBar = false;
+  }
 }
